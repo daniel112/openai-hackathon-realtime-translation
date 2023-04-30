d59c7a6cafa3fee6019b3e7db394b4ae4ebec124
danielyo <danielyo112@gmail.com>
2023-04-29 21:34:58 -0700

    translating chat locally based on selected language

--- frontend/src/screen/demo-textchat/DemoTextChatComponent.tsx
+++ frontend/src/screen/demo-textchat/DemoTextChatComponent.tsx
@@ -16,6 +16,8 @@ import { createAutoRefreshingCredential } from "./services/credential";
 import { CommunicationUserIdentifier } from "@azure/communication-common";
 import { Box, Button, HStack, VStack } from "@chakra-ui/react";
 import { Text } from "@chakra-ui/react";
+import React from "react";
+import { translateTranscript } from "../../services/openai";
 
 interface ChatScreenProps {
   token: string;
@@ -117,54 +119,7 @@ export const TextChatComponent = (props: ChatScreenProps): JSX.Element => {
           <ChatComposite
             adapter={adapter}
             onRenderMessage={(props) => {
-              /**
-               * When a user join. display some system message
-               */
-              if (props.message.messageType === "system") {
-                const systemMessage = props.message as SystemMessage;
-                if (systemMessage.systemMessageType === "participantAdded") {
-                  const newUserDisplayName =
-                    systemMessage.participants[0].displayName;
-                  return <Text>{newUserDisplayName} joined</Text>;
-                }
-              }
-              if (props.message.messageType === "custom") {
-                console.log({ custom: props });
-              }
-              if (props.message.messageType === "chat") {
-                const chatMessage = props.message as ChatMessage;
-                if (chatMessage.mine) {
-                  return (
-                    <Box
-                      bg={"messenger.400"}
-                      p={4}
-                      color={"white"}
-                      borderRadius={8}
-                      maxW={"30%"}
-                    >
-                      {chatMessage.content}
-                    </Box>
-                  );
-                } else {
-                  return (
-                    <Box
-                      bg={"blackAlpha.300"}
-                      p={4}
-                      color={"white"}
-                      borderRadius={8}
-                      maxW={"30%"}
-                    >
-                      <VStack alignItems={"flex-start"}>
-                        <Text color={"gray.400"}>
-                          {chatMessage.senderDisplayName}
-                        </Text>
-                        <Text>{chatMessage.content}</Text>
-                      </VStack>
-                    </Box>
-                  );
-                }
-              }
-              return <div></div>;
+              return <RenderMessage languageIso={languageIso} {...props} />;
             }}
             fluentTheme={darkTheme}
             options={{
@@ -177,4 +132,73 @@ export const TextChatComponent = (props: ChatScreenProps): JSX.Element => {
     );
   }
   return <>Initializing...</>;
+};
+
+const RenderMessage = (props: any) => {
+  const { languageIso } = props;
+  // dangerously accessing the message content. but its fine. its a demo
+  const [translatedMessage, setTranslatedMessage] = React.useState<string>(
+    props.message.content
+  );
+  React.useEffect(() => {
+    const translate = async () => {
+      const translated = await translateTranscript({
+        text: props.message.content,
+        toLanguage: languageIso,
+        fromLanguage: "this",
+      });
+      setTranslatedMessage(translated.text!);
+    };
+    // translate the incoming message
+    // we are assuming that my own message is in the language that I chose
+    if (props.message.messageType === "chat" && !props.message.mine) {
+      translate();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  /**
+   * When a user join. display some system message
+   */
+  if (props.message.messageType === "system") {
+    const systemMessage = props.message as SystemMessage;
+    if (systemMessage.systemMessageType === "participantAdded") {
+      const newUserDisplayName = systemMessage.participants[0].displayName;
+      return <Text>{newUserDisplayName} joined</Text>;
+    }
+  }
+  // if (props.message.messageType === "custom") {
+  //   console.log({ custom: props });
+  // }
+  if (props.message.messageType === "chat") {
+    const chatMessage = props.message as ChatMessage;
+    if (chatMessage.mine) {
+      return (
+        <Box
+          bg={"messenger.400"}
+          p={4}
+          color={"white"}
+          borderRadius={8}
+          maxW={"30%"}
+        >
+          {translatedMessage}
+        </Box>
+      );
+    } else {
+      return (
+        <Box
+          bg={"blackAlpha.300"}
+          p={4}
+          color={"white"}
+          borderRadius={8}
+          maxW={"30%"}
+        >
+          <VStack alignItems={"flex-start"}>
+            <Text color={"gray.400"}>{chatMessage.senderDisplayName}</Text>
+            <Text>{translatedMessage}</Text>
+          </VStack>
+        </Box>
+      );
+    }
+  }
+  return <div></div>;
 };
